5a3746c97e42e875e9a2f25a9d23dedf357e3d9c
melody1214 <jiwoongwon@hanyang.ac.kr>
2020-02-19 03:43:13 +0900

    c++: modify bitset

--- c++/03_bitset.cpp
+++ c++/03_bitset.cpp
@@ -17,7 +17,9 @@ int main()
     cout << "bit2: " << bit2 << endl;
 
     /*
-    C++에서는 std 클래스에 입출력을 위한 shift 연산자가 정의되어 있다.
+    C++에서는 iostream에 istream, ostream 클래스가 있고, istream에는 객체 cin, ostream에는 cout이 선언되어 있다.
+    cin, cout에 대해서 shift 연산자와 동일한 형태 (>>, <<)의 연산자 함수가 정의되어있고,
+    두 객체에 대해서 >> 또는 << 연산자를 입력하면 비트 연산이 아닌 입력이나 출력이 이루어진다.
     따라서 cout을 이용하여 bit 연산 결과를 출력하고 싶다면 bit 연산은 bitset 끼리 묶어주어야한다.
     bitset 끼리 괄호로 묶어주면 그 안에서는 bit 연산을 수행하지만 밖에서는 출력을 위한 연산자로 인식한다.
     가독성을 높이려면 bit 연산은 따로 수행하고 결과를 cout에 포함해주면 좋을듯 하다.
